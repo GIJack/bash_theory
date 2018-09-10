541d1dfe8ac3683e7f5e326f39325e85d517b546
GI_Jack <iamjacksemail@hackermail.com>
2018-09-10 17:22:18 -0400

    re-wrote this into parse_enviroment() and parse_config()

--- parse_conf.sh
+++ parse_conf.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 #
-# This script is for safely, or less dangerously parsing a config file from bash
-# It is possible to source a file with variables, but this can lead to code
-# execution, and funciton replacement. Sourcing unknown files can be dangerous.
-# This is a demonstration of a safer config file parser. scrub config options
+# These two functions are for parse settings and configuration files. The first
+# "parse_enviroment()", parses a shell enviroment file, a text file with a set
+# of key=value instructions that are loaded as shell variables in the current
+# script. The second "parse_config()", loads the same into a bash array called
+# "CONFIG"
+#
+# These functions santize inputs and respect the "#" comment character.
 #
 # There are two functions :
 
-# parse_config() imports key=pair values directly as bash variables. this will
-# override whatever values previously set. use as parse_config <filename>
+# parse_enviroment() - is to parse an enviroment variable file, after scrubbing
+#			sanitizing inputs. It is a drop in replacement for
+#			source. NOTE: all variables will be made uppercase.
+#	Usage:
+#	parse_enviroment [file]
 
-# secure_parse_config() imports to named array called CONFIG
+# parse_config() -	parses a key=value value config into an named array named
+#			config after sanitizing data. the default name for the
+#			array is CONFIG, although it can be specified. This
+#			strips all non alphanumer characters from the key, and
+#			removes	escaping characters from the the value.
+#	Usage:
+#	parse_config [file]
 
-# error codes 2-no input 1-ran with error
+# error codes 2-no input 1-ran with error 0-ran successfully
 
-parse_config(){
-  # parse a key=pair configuration file. Less Dangerously. one parameter, an
-  # input file
+parse_enviroment(){
+  # parse a key=pair shell enviroment file. NOTE all keys will be made UPPERCASE
+  # variables. in parent script.
+
   local infile="${@}"
   local safe_config=$(mktemp)
   local key=""
@@ -25,37 +38,43 @@ parse_config(){
   [ -f ${infile} ] || return 2 # infile is not a file
   # Now we have an array of file lines
   readarray file_lines < "${infile}" || return 1 # error proccessing
+
   for line in ${file_lines[@]};do
     # Remove comments
     [ ${line} == "#" ]; continue
     line=$(cut -d "#" -f 1 <<< ${line} )
+
     # Split key and value from lines
     key=$(cut -d "=" -f 1 <<< ${line} )
     value=$(cut -d "=" -f 2 <<< ${line} )
-    # Parse key. All variables uppercase, remove spaces, all non alphanumeric
+
+    # Parse key. Make the Key uppercase, remove spaces and all non-alphanumeric
     # characters
     key=$(key^^)
     key=${key// /}
     key=$(tr -cd "[:alnum:]" <<< $key)
+
     # Parse value. Remove anything that can escape a variable and run code.
-    value=$(tr -d ";|&" <<< $value )
+    value=$(tr -d ";|&()" <<< $value )
+
     # Zero check. If after cleaning either the key or value is null, then
     # write nothing
     [ -z $key ] && continue
     [ -z $value ] && continue
+
     # write sanitized values to temp file
     echo "${key}=${value}" >> ${safe_config}
   done
 
-  #Now, we can import the cleaned config
+  #Now, we can import the cleaned config and then delete it.
   source ${safe_config}
   rm $(safe_config)
 }
 
 ################################################################################
-secure_parse_config(){
-  # parse a key=pair configuration file. Less Dangerously. outputs to an array
-  # called CONFIG
+parse_config(){
+  # parse a key=pair configuration file into an array called CONFIG.
+  local array_name=
   declare -A CONFIG
   local infile="${@}"
   local key=""
@@ -69,21 +88,24 @@ secure_parse_config(){
     # Remove comments
     [ ${line} == "#" ]; continue
     line=$(cut -d "#" -f 1 <<< ${line} )
+
     # Split key and value from lines
     key=$(cut -d "=" -f 1 <<< ${line} )
     value=$(cut -d "=" -f 2 <<< ${line} )
+
     # Parse key. All variables uppercase, remove spaces, all non alphanumeric
     # characters
-    key=$(key^^)
     key=${key// /}
     key=$(tr -cd "[:alnum:]" <<< $key)
     # Parse value. Remove anything that can escape a variable and run code.
     value=$(tr -d ";|&" <<< $value )
-    # Zero check. If after cleaning either the key or value is null, then
-    # write nothing
+ 
+   # Zero check. If after cleaning either the key or value is null, then
+    # do nothing
     [ -z $key ] && continue
     [ -z $value ] && continue
-    # write sanitized values to temp file
-    CONFIG[${key}]="${value}"
+
+    # Enter sanitized values to array "CONFIG"
+    CONFIG["${key}"]="${value}"
   done
 }
